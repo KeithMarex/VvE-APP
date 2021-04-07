47185276c667b3ab70b9ee32c5e06534f6aad901
Jan <s1120303@student.hsleiden.nl>
2021-04-07 14:13:22 +0200

    Created ticket model with properties

---
+++ API/models/Ticket.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const TicketSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'A title is required'],
+
+    },
+    description: {
+        type: String,
+        required: [true, 'A description is required']
+    },
+    images: [{
+        type: Schema.Types.ObjectId,
+        ref: "Image"
+    }],
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    assignee: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    status: {
+        type: Schema.Types.ObjectId,
+        ref: "Status"
+    },
+    Comments: [{
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+    }],
+    Tag: {
+        type: Schema.Types.ObjectId,
+        ref: "Tag"
+    }
+});
+export default mongoose.model('Ticket', TicketSchema);
