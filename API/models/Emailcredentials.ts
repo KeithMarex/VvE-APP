7658ccb75999ce5d3bd31c359ab6ffbf805f9dbe
Jan <s1120303@student.hsleiden.nl>
2021-04-07 15:04:22 +0200

    Created emailcredentials model with properties

---
+++ API/models/Emailcredentials.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+import { validateEmail } from '~/validators/emailValidator';
+
+const Schema = mongoose.Schema;
+
+const Emailcredentials = new Schema({
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: "Email does already exist",
+        lowercase: true,
+        trim: true,
+        validate: {
+            validator: validateEmail,
+            message: 'Please fill a valid email address'
+        },
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    host: {
+        type: String,
+        required: [true, 'The host is required']
+    },
+    port: {
+        type: Number,
+        required: [true, `The port is required`]
+    }
+}, { timestamps: true });
+
+export default mongoose.model('Emailcredentials', Emailcredentials);
