0a9be43792bd5198702da2346fc70eefbe69507f
Jan <s1120303@student.hsleiden.nl>
2021-04-07 16:14:04 +0200

    Added getTicket function

--- API/controllers/TicketController.ts
+++ API/controllers/TicketController.ts
@@ -10,3 +10,15 @@ export const getTickets = async (req, res) => {
         res.status(status).json({message: err})
     });
 }
+
+export const getTicket = async (req, res) => {
+    const id = req.params.id;
+    Ticket.findById(id)
+    .then(result => {
+        res.status(200).send(result);
+    })
+    .catch(err => {
+        const status = err.statusCode || 500;
+        res.status(status).json({message: err})
+    });
+}
