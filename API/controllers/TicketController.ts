a88cc34e815b0c05c20733ff59d566983cb41665
Jan <s1120303@student.hsleiden.nl>
2021-04-07 16:29:10 +0200

    Added putTicket function

--- API/controllers/TicketController.ts
+++ API/controllers/TicketController.ts
@@ -34,3 +34,21 @@ export const postTicket = async (req, res) => {
         res.status(status).json({message: err})
     });
 }
+
+export const putTicket = async (req, res) => {
+    const id = req.params.id;
+    const ticket: any = new Ticket(req.body);
+    Ticket.updateOne({ _id: id }, {
+        assignee: ticket.assignee,
+        status: ticket.status,
+        comments: ticket.comments,
+        tag: ticket.tag
+     })
+    .then(result => {
+        res.status(200).send(result);
+    })
+    .catch(err => {
+        const status = err.statusCode || 500;
+        res.status(status).json({message: err})
+    });
+}
