f1e30e4bb6f7152ca0097d4c13d5b4795af3ed3d
Jan <s1120303@student.hsleiden.nl>
2021-04-08 15:46:44 +0200

    Added post sample

--- API/test/Sample.spec.ts
+++ API/test/Sample.spec.ts
@@ -10,22 +10,39 @@ const server = supertest.agent("http://localhost:3000");
 // You could call this the test class
 describe("SAMPLE unit test for requests",function(){
 
-    // This is a single test
-    it("should return json",function(done){
+  // This is a single test
+  it("should return json",function(done){
 
-      // Making a request to the api
-      server
-      .get("/ticket") // Get > uri
-      .expect("Content-type",/json/) // Says content type
-      .expect(200) // This is the HTTP response
-      .end(function(err,res){ // This function has all the values that are passed
-        // HTTP status should be 200
-        should(res.status).equal(200);
-        // Error is not in the body when there is none
-        // Check if error doesn't exists (undefined)
-        should(res.body.error).equal(false || undefined);
-        done(); // End of test
-      });
+    // Making a get request to the api
+    server
+    .get("/ticket") // Get > uri
+    .expect("Content-type",/json/) // Says content type
+    .expect(200) // This is the HTTP response
+    .end(function(err,res){ // This function has all the values that are passed
+      // HTTP status should be 200
+      should(res.status).equal(200);
+      // Error is not in the body when there is none
+      // Check if error doesn't exists (undefined)
+      should(res.body.error).equal(false || undefined);
+      done(); // End of test
     });
+  });
+
+  // This is a single test
+  it("should add json",function(done){
+
+    // Making a post request to the api
+    server
+    .post('/ticket')
+    .send({title: "sample", description: "This is a sample"})
+    .expect("Content-type",/json/)
+    .expect(200)
+    .end(function(err,res){
+      should(res.status).equal(200);
+      should(res.body.error).equal(false || undefined);
+      should(res.body.data).equal(30);
+      done();
+    });
+  });
 
   });
