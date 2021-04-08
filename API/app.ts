09f57906328ed876881c5c0379b67dd3924ced2f
Jan <s1120303@student.hsleiden.nl>
2021-04-08 10:32:47 +0200

    Added logging to startup

--- API/app.ts
+++ API/app.ts
@@ -9,6 +9,7 @@ import swaggerUI from 'swagger-ui-express';
 
 import swaggerDocument from './util/swagger.json';
 import { isAuth } from './middleware/isAuth';
+import { successLog } from '~/util/Logger';
 
 import UserRouter from './routes/UserRouter';
 
@@ -33,5 +34,9 @@ mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTop
     .then(result => {
         app.listen(process.env.PORT);
         console.log(`Running on port ${process.env.PORT}`);
+        successLog.log({
+            level: 'low',
+            message: "Api is running"
+        })
     })
     .catch(err => console.log(err));
