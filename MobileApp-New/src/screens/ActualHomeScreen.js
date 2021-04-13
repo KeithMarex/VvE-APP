ba346a0caa4131ddd617905b25c2826e26c55601
Jeroen <jegbol01@gmail.com>
2021-04-13 20:35:54 +0200

    Styling of home page

--- MobileApp-New/src/screens/ActualHomeScreen.js
+++ MobileApp-New/src/screens/ActualHomeScreen.js
@@ -1,51 +1,44 @@
 import React, { useState } from 'react'
 import { Text, StyleSheet, View, Image, Dimensions, ScrollView, SafeAreaView } from 'react-native'
 import StyledText from '../components/StyledText'
-import { BoxShadow } from 'react-native-shadow'
 
 const window = Dimensions.get('window')
 
 const HomeScreen = (props) => {
-    // const [introHeight, setIntroHeight] = useState(0)
-
-    // console.log(introHeight)
-
-    const shadowOpt = {
-        width: window.width - 60,
-        height: 10,
-        color: "#111111",
-        border: 16,
-        radius: 10,
-        opacity: 0.04,
-        x: 0,
-        y: 8,
-        style: { marginVertical:5 }
-    }
-
     return (
-        // <View style={styles.view}>
-        //     <Image style={styles.logo} source={require('../resources/images/de-nieuwe-wereld-logo.png')} resizeMode="contain" />
-        //     <TextInput style={styles.input} onChangeText={onChangeName} value={username} placeholder="Email" />
-        //     <TextInput style={styles.input} onChangeText={onChangePass} value={pass} placeholder="Password" />
-        //     <TouchableOpacity style={styles.loginButton} onPress={() => props.navigation.navigate('Component')}><Text style={styles.text}>Login</Text></TouchableOpacity>
-        //     <TouchableOpacity style={styles.passForgotBtn} onPress={() => props.navigation.navigate('List')}><Text style={styles.passForgot}>Wachtwoord vergeten?</Text></TouchableOpacity>
-        // </View>
         <SafeAreaView style={styles.root}>
             <ScrollView style={styles.scrollView}>
                 <View style={styles.home}>
                     <Image style={styles.logo} source={require('../resources/images/de-nieuwe-wereld-logo.png')} resizeMode="contain" />
-                    <BoxShadow setting={shadowOpt}>
-                        <View style={styles.intro}>
-                            <StyledText inputStyle={styles.introWelcome}>Welkom</StyledText>
-                            <StyledText inputStyle={styles.introName}>Hicham Ben Yessef</StyledText>
-                            <View style={styles.introMessage}>
-                                <StyledText inputStyle={styles.introMessageText}>1 nieuwe reactie ></StyledText>
-                            </View>
-                            <View style={styles.introMessage}>
-                                <StyledText inputStyle={[styles.introMessageText, styles.introMessageTextAgenda]}>3 nabije agendapunten ></StyledText>
+
+                    <View style={[styles.homeSection, styles.intro]}>
+                        <StyledText inputStyle={styles.introWelcome}>Welkom</StyledText>
+                        <StyledText inputStyle={styles.introName}>Hicham Ben Yessef</StyledText>
+                        <View style={styles.introMessage}>
+                            <StyledText inputStyle={[styles.introMessageText, styles.introMessageTextComments]}>1 nieuwe reactie ></StyledText>
+                        </View>
+                        <View style={styles.introMessage}>
+                            <StyledText inputStyle={[styles.introMessageText, styles.introMessageTextAgenda]}>3 nabije agendapunten ></StyledText>
+                        </View>
+                    </View>
+
+                    <View style={styles.homeSectionWrapper}>
+                        <StyledText inputStyle={styles.sectionHeader} theme={'sectionHeader'}>Binnenkort</StyledText>
+                        <View style={[styles.homeSection, styles.agenda]}>
+                            <StyledText inputStyle={styles.agendaItemName}>Maandelijkse vergadering</StyledText>
+                            <View>
+                                <StyledText inputStyle={styles.agendaItemDate}>Za. 11 maart 16:00 - 17:30</StyledText>
                             </View>
                         </View>
-                    </BoxShadow>
+                    </View>
+
+                    <View style={styles.homeSectionWrapper}>
+                        <StyledText inputStyle={styles.sectionHeader} theme={'sectionHeader'}>Recent nieuws</StyledText>
+                        <View style={[styles.homeSection, styles.news]}>
+                            <Image style={styles.newsImage} source={require('../resources/images/news-placeholder.png')}/>
+                            <View style={styles.newsImageOverlay}/>
+                        </View>
+                    </View>
                 </View>
             </ScrollView>
         </SafeAreaView>
@@ -60,14 +53,10 @@ const styles = StyleSheet.create({
         backgroundColor: '#F7F7FC',
         height: '100%',
     },
-    // scrollView: {
-    //     width: '100%',
-    // },
     home: {
         justifyContent: 'center',
         alignItems: 'center',
         flex: 1,
-        position: 'relative',
         padding: 30
     },
     logo: {
@@ -75,16 +64,26 @@ const styles = StyleSheet.create({
         height: window.height / 6,
         marginBottom: 10
     },
-
-    intro: {
+    homeSection: {
         width: '100%',
         backgroundColor: '#FCFCFC',
         borderRadius: 20,
+        borderColor: '#f8f8f8',
+        borderWidth: 5
+    },
+    homeSectionWrapper: {
+        marginTop: 40,
+        width: '100%'
+    },
+    sectionHeader: {
+        marginBottom: 10
+    },
+
+    intro: {
         paddingRight: 10,
         paddingLeft: 10,
         paddingTop: 21,
         paddingBottom: 25,
-        position: 'relative'
     },
     introWelcome: {
         fontSize: 13,
@@ -100,11 +99,42 @@ const styles = StyleSheet.create({
         marginTop: 10
     },
     introMessageText: {
-        fontSize: 16,
         fontWeight: '300'
     },
+    introMessageTextComments: {
+        fontSize: 19,
+    },
     introMessageTextAgenda: {
-        fontSize: 14
+        fontSize: 16
+    },
+
+    agenda: {
+        paddingTop: 15,
+        paddingBottom: 18
+    },
+    agendaItemName: {
+        fontSize: 17,
+        marginBottom: 10
+    },
+    agendaItemDate: {
+        color: '#A0CAE8',
+        fontSize: 14,
+    },
+
+    news: {
+        position: 'relative',
+    },
+    newsImage: {
+        width: '100%',
+        height: 225,
+        borderRadius: 20
+    },
+    newsImageOverlay: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        borderRadius: 20,
+        backgroundColor: 'linear-gradient(0.24deg, lighten(rgba($primary, .8), 5%) .08%, lighten(rgba($primary, .7), 80%) 99.92%)'
     }
 
 });
