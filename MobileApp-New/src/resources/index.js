7a04e3916322a9d36e624617fcd001a1a68034ea
Jeroen <jegbol01@gmail.com>
2021-04-15 12:26:30 +0200

    Added home svg

--- MobileApp-New/src/resources/index.js
+++ MobileApp-New/src/resources/index.js
@@ -1,7 +1,9 @@
 import Comment from './icons/Comment.svg'
 import Calendar from './icons/Calandar.svg'
+import Home from './icons/Home.svg'
 
 export {
     Calendar,
-    Comment
+    Comment,
+    Home
 }
