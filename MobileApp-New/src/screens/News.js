c4e25f30fe8c1cec173a57139b846dbdee3afb92
Jeroen <jegbol01@gmail.com>
2021-04-20 22:50:03 +0200

    Styled news page

--- MobileApp-New/src/screens/News.js
+++ MobileApp-New/src/screens/News.js
@@ -18,6 +18,7 @@ const News = () => {
                     <NewsShowcase />
 
                     <View style={styles.newsList}>
+
                         <View style={styles.newsItem}>
                             <View style={styles.newsThumbnailWrapper}>
                                 <Image
@@ -25,18 +26,39 @@ const News = () => {
                                     source={require('../resources/images/news-placeholder.png')}
                                 />
                             </View>
-                            <View>
+                            <View style={styles.newsTextWrapper}>
                                 <View style={styles.newsTextTopWrapper}>
                                     <View style={styles.newsTextTopOrganization}>
-                                        <HomeIcon stroke={'#A0CAE8'} width={10} height={10} />
+                                        <HomeIcon stroke={'#A0CAE8'} width={8} height={8} />
                                         <StyledText inputStyle={styles.newsTextTop}>De Nieuwe Wereld</StyledText>
                                     </View>
                                     <StyledText inputStyle={styles.newsTextTopLine}>|</StyledText>
                                     <StyledText inputStyle={styles.newsTextTop}>25/03/2021</StyledText>
                                 </View>
-                                <StyledText>Gastoptreden van DJ Billybool</StyledText>
+                                <StyledText inputStyle={styles.newsTitle}>Dit is een nieuwsartikel</StyledText>
                             </View>
                         </View>
+
+                        <View style={styles.newsItem}>
+                            <View style={styles.newsThumbnailWrapper}>
+                                <Image
+                                    style={styles.newsThumbnail}
+                                    source={require('../resources/images/news-placeholder.png')}
+                                />
+                            </View>
+                            <View style={styles.newsTextWrapper}>
+                                <View style={styles.newsTextTopWrapper}>
+                                    <View style={styles.newsTextTopOrganization}>
+                                        <HomeIcon stroke={'#A0CAE8'} width={8} height={8} />
+                                        <StyledText inputStyle={styles.newsTextTop}>De Nieuwe Wereld</StyledText>
+                                    </View>
+                                    <StyledText inputStyle={styles.newsTextTopLine}>|</StyledText>
+                                    <StyledText inputStyle={styles.newsTextTop}>25/03/2021</StyledText>
+                                </View>
+                                <StyledText inputStyle={styles.newsTitle}>Dit is nog een interessant nieuwsartikel</StyledText>
+                            </View>
+                        </View>
+
                     </View>
                 </View>
             </ScrollView>
@@ -62,25 +84,29 @@ const styles = StyleSheet.create({
     pageTitle: {
         marginBottom: window.height / 40,
     },
+    sectionHeader: {
+      marginBottom: '2%'
+    },
 
     newsList: {
         width: '100%'
     },
     newsItem: {
         width: '100%',
+        maxWidth: '100%',
         backgroundColor: '#fff',
         borderRadius: 20,
         borderColor: '#f8f8f8',
         borderWidth: 5,
-        marginBottom: 8,
         paddingVertical: '3%',
-        paddingHorizontal: '4%',
-        flexDirection: 'row'
+        paddingHorizontal: '3%',
+        flexDirection: 'row',
+        alignItems: 'center'
     },
     newsThumbnailWrapper: {
         width: 100,
         height: 60,
-        borderRadius: 10,
+        borderRadius: 10
     },
     newsThumbnail: {
         borderRadius: 5,
@@ -88,23 +114,33 @@ const styles = StyleSheet.create({
         height: '100%'
     },
 
+    newsTextWrapper: {
+        marginLeft: '3%',
+        flex: 1
+    },
     newsTextTopWrapper: {
         flexDirection: 'row',
     },
     newsTextTop: {
         color: '#6E7191',
-        fontSize: 10,
-        marginRight: 8,
-        marginLeft: 8,
+        fontSize: 8,
+        marginHorizontal: 6
     },
     newsTextTopLine: {
-        fontSize: 10,
+        fontSize: 8,
         color: '#6E7191'
     },
     newsTextTopOrganization: {
         flexDirection: 'row',
         alignItems: 'center'
     },
+    
+    newsTitle: {
+        marginTop: 2,
+        textAlign: 'left',
+        fontSize: 14,
+        color: '#4E4B66'
+    }
 })
 
 export default News
