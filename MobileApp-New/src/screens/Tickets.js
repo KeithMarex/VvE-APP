46fd789c5904f78f3f471d78c2262ad0370aa559
Jeroen <jegbol01@gmail.com>
2021-05-18 13:08:22 +0200

    Added fake ticket object to Tickets page that is used as input for TicketsListItem components

--- MobileApp-New/src/screens/Tickets.js
+++ MobileApp-New/src/screens/Tickets.js
@@ -3,10 +3,19 @@ import React from 'react'
 import StyledText from '../components/StyledText'
 import { Logo, CommentIcon } from '../resources'
 import PageActionButton from '../components/PageActionButton'
+import TicketsListItem from '../components/TicketsListItem'
 
 const window = Dimensions.get('window')
 
 const Tickets = (props) => {
+    const ticket = {
+        title: 'Titel van melding',
+        description: 'Dit is een kleine beschrijving van de melding die is gedaan.',
+        comments: ['1', '2'],
+        lastUpdate: '13 mei 2021 15:30',
+        status: 'In behandeling'
+    }
+
     return (
         <SafeAreaView style={styles.root}>
             <ScrollView style={styles.scrollView}>
@@ -16,69 +25,12 @@ const Tickets = (props) => {
                     <PageActionButton icon={'plus'} text={'Aanmaken'}/>
 
                     <View>
-                        <View style={styles.ticket}>
-
-                            <View style={styles.ticketHeader}>
-                                <StyledText inputStyle={styles.ticketTitle} theme={'cardHeader'}>Titel van melding</StyledText>
-                                <View style={styles.ticketCommentCount}>
-                                    <StyledText inputStyle={styles.ticketCommentCountAmount}>0</StyledText>
-                                    <CommentIcon width={18} height={18} stroke={'#4E4B66'} />
-                                </View>
-                            </View>
-
-                            <StyledText inputStyle={styles.ticketDescription}>
-                                Dit is een kleine beschrijving van de melding die is gedaan.
-                            </StyledText>
-
-                            <StyledText inputStyle={styles.ticketStatus}>Status: In behandeling</StyledText>
-
-                            <View style={styles.ticketBottom}>
-                                <View style={styles.lastUpdate}>
-                                    <StyledText inputStyle={styles.lastUpdateText}>Laatste wijziging:</StyledText>
-                                    <StyledText inputStyle={styles.lastUpdateText}>13 mei 2021 15:30</StyledText>
-                                </View>
-
-                                <TouchableOpacity style={styles.ticketBtn} onPress={() => alert('Tapped ticket button')} >
-                                    <StyledText inputStyle={styles.ticketBtnText}>
-                                        Meer info &#62;
-                                    </StyledText>
-                                </TouchableOpacity>
-                            </View>
-
-                        </View>
-
-                        <View style={styles.ticket}>
-
-                            <View style={styles.ticketHeader}>
-                                <StyledText inputStyle={styles.ticketTitle} theme={'cardHeader'}>Titel van melding</StyledText>
-                                <View style={styles.ticketCommentCount}>
-                                    <StyledText inputStyle={styles.ticketCommentCountAmount}>0</StyledText>
-                                    <CommentIcon width={18} height={18} stroke={'#4E4B66'} />
-                                </View>
-                            </View>
-
-                            <StyledText inputStyle={styles.ticketDescription}>
-                                Dit is een kleine beschrijving van de melding die is gedaan.
-                            </StyledText>
-
-                            <StyledText inputStyle={styles.ticketStatus}>Status: In behandeling</StyledText>
-
-                            <View style={styles.ticketBottom}>
-                                <View style={styles.lastUpdate}>
-                                    <StyledText inputStyle={styles.lastUpdateText}>Laatste wijziging:</StyledText>
-                                    <StyledText inputStyle={styles.lastUpdateText}>13 mei 2021 15:30</StyledText>
-                                </View>
-
-                                <TouchableOpacity style={styles.ticketBtn} onPress={() => alert('Tapped ticket button')}>
-                                    <StyledText inputStyle={styles.ticketBtnText}>
-                                        Meer info &#62;
-                                    </StyledText>
-                                </TouchableOpacity>
-                            </View>
-
-                        </View>
-
+                        <TicketsListItem ticket={ticket}/>
+                        <TicketsListItem ticket={ticket}/>
+                        <TicketsListItem ticket={ticket}/>
                     </View>
+
+
 
                 </View>
             </ScrollView>
@@ -122,71 +74,6 @@ const styles = StyleSheet.create({
         fontSize: 11,
         color: 'black'
     },
-
-    ticket: {
-        width: '100%',
-        backgroundColor: '#fff',
-        borderRadius: 20,
-        borderColor: '#f8f8f8',
-        borderWidth: 5,
-        paddingVertical: 20,
-        paddingHorizontal: '7%',
-        marginBottom: 8
-    },
-    ticketHeader: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 10,
-        justifyContent: 'space-between'
-    },
-    ticketTitle: {
-
-    },
-    ticketCommentCount: {
-        flexDirection: 'row'
-    },
-    ticketCommentCountAmount: {
-        color: '#4E4B66',
-        fontSize: 16,
-        marginRight: 5
-    },
-    ticketDescription: {
-        color: '#6E7191',
-        fontSize: 13,
-        textAlign: 'left',
-        marginBottom: 10,
-    },
-    ticketStatus: {
-        textAlign: 'left',
-        color: '#14142B',
-        marginBottom: 10
-    },
-    ticketBottom: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-    },
-    lastUpdateText: {
-        textAlign: 'left',
-        fontSize: 10,
-        color: '#6E7191',
-        letterSpacing: 0.5,
-        marginBottom: 3
-    },
-    ticketBtn: {
-        borderColor: '#A0CAE8',
-        borderWidth: 2,
-        borderRadius: 40,
-        justifyContent: 'center',
-        paddingHorizontal: 8,
-        paddingVertical: 4
-    },
-    ticketBtnText: {
-        color: '#A0CAE8',
-        fontSize: 14,
-        fontWeight: 'bold'
-    }
-
 })
 
 export default Tickets;
