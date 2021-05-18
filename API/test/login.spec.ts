5d4e1dac8f252d8385b81aee2acc29b49e6b567c
Jan <s1120303@student.hsleiden.nl>
2021-05-18 11:06:33 +0200

    Added login incorrect password test

--- API/test/login.spec.ts
+++ API/test/login.spec.ts
@@ -8,7 +8,6 @@ const server = supertest.agent("http://localhost:3000");
 // Unit tests
 describe("Login unit test for requests",function(){
 
-  // This is a single test
   it("should login user with tokens",function(done){
 
     // Making a post request to the api
@@ -25,7 +24,6 @@ describe("Login unit test for requests",function(){
     });
   });
 
-  // This is a single test
   it("should not login user with incorrect email",function(done){
 
     // Making a post request to the api
@@ -38,6 +36,23 @@ describe("Login unit test for requests",function(){
       should(res.status).equal(500);
       should.not.exist(res.cookies['access-token']);
       should.not.exist(res.cookies['refresh-token']);
+      done();
+    });
+  });
+
+  it("should not login user with incorrect password",function(done){
+
+    // Making a post request to the api
+    server
+    .post("/login")
+    .send({ email: 'test@test.com', password: 'wrongpassword' })
+    .expect("Content-type",/json/) // Says content type
+    .expect(401)
+    .end( function(err,res) {
+      should(res.status).equal(401);
+      should.not.exist(res.cookies['access-token']);
+      should.not.exist(res.cookies['refresh-token']);
+      should.equal(res.message, 'Password incorrect')
       done();
     });
   });
