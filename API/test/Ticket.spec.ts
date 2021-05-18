67b7f99f471925b438be0960ee54c7df05d655a5
Jan <s1120303@student.hsleiden.nl>
2021-05-18 19:27:30 +0200

    Added getTickets admin test

--- API/test/Ticket.spec.ts
+++ API/test/Ticket.spec.ts
@@ -9,10 +9,20 @@ const server = supertest.agent("http://localhost:3000");
 
 describe("Ticket unit test for requests",function(){
 
-  // This is a single test
   it("should return tickets of user",function(done){
 
-    // Get request tickets for user
+    server
+    .get("/ticket")
+    .expect("Content-type",/json/)
+    .expect(200)
+    .end(function(err,res){
+      should(res.status).equal(200);
+      done();
+    });
+  });
+
+  it("should return tickets of admin",function(done){
+
     server
     .get("/ticket")
     .expect("Content-type",/json/)
