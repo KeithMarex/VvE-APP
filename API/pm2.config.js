571c0cdfbf9f9bfb7cf5feb6a0d094f85496ee6d
Jan <s1120303@student.hsleiden.nl>
2021-05-19 10:44:14 +0200

    Added max memory

--- API/pm2.config.js
+++ API/pm2.config.js
@@ -4,6 +4,7 @@ module.exports = {
             name: 'VvE Api',
             script: './build/app.js',
             watch: true,
+            max_memory_restart: '250M',
             env: {
                 githook: {
                     command: 'git pull && npm install && tsc'
