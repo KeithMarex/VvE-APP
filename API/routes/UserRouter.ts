34b89ca4daa6f43826345f77fb07dc7e45d999df
Jan <s1120303@student.hsleiden.nl>
2021-05-19 11:07:14 +0200

    Renamed IsAuth

--- API/routes/UserRouter.ts
+++ API/routes/UserRouter.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { login, register, getUser } from '../controllers/UserController';
-import { isAuth } from '../middleware/sAuth';
+import { isAuth } from '../middleware/IsAuth';
 
 const router = Router();
 
