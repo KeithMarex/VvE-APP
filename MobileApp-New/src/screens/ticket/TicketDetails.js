05f243eaade173f4f423b0638cbf67844cc89715
Jeroen <jegbol01@gmail.com>
2021-05-19 11:25:42 +0200

    Initialised ticket details page

--- MobileApp-New/src/screens/ticket/TicketDetails.js
+++ MobileApp-New/src/screens/ticket/TicketDetails.js
@@ -1,55 +1,20 @@
-import {SafeAreaView, StyleSheet, ScrollView, View, Dimensions, TouchableOpacity} from 'react-native'
-import React, {useEffect, useState} from 'react'
+import {SafeAreaView, StyleSheet, ScrollView, View, Dimensions} from 'react-native'
+import React, {} from 'react'
 import StyledText from '../../components/StyledText'
 import { Logo } from '../../resources'
-import PageActionButton from '../../components/PageActionButton'
-import TicketsListItem from '../../components/TicketsListItem'
-import axios from 'axios'
 
 const window = Dimensions.get('window')
 
 const TicketDetails = (props) => {
-    const [tickets, setTickets] = useState([])
-
-    useEffect(() => {
-        fetchTickets()
-    }, [])
-
-    const fetchTickets = () => {
-        setTickets([
-            {
-                title: 'Titel van melding',
-                description: 'Dit is een kleine beschrijving van de melding die is gedaan.',
-                comments: ['1', '2'],
-                lastUpdate: '13 mei 2021 15:30',
-                status: 'In behandeling'
-            }
-        ])
-    }
-
-    const ticketsListEl = []
-    for (let i = 0; i < tickets.length; i++) {
-        ticketsListEl.push(
-            <TicketsListItem ticket={tickets[i]} key={i}/>
-        )
-    }
-
     return (
         <SafeAreaView style={styles.root}>
             <ScrollView style={styles.scrollView}>
-                <View style={styles.tickets}>
+                <View style={styles.ticketDetails}>
                     <Logo style={styles.logo} width={window.width / 10 * 5} />
-                    <StyledText inputStyle={styles.pageTitle} theme={'pageTitle'}>Meldingen</StyledText>
-                    <TouchableOpacity onPress={() => props.navigation.navigate('Create')}>
-                        <PageActionButton icon={'plus'} text={'Aanmaken'}/>
-                    </TouchableOpacity>
+                    <StyledText inputStyle={styles.pageTitle} theme={'pageTitle'}>Titel van melding</StyledText>
 
                     <View>
-                        {ticketsListEl.length > 0 ? ticketsListEl : (
-                            <StyledText inputStyle={styles.noTickets}>
-                                U heeft nog geen meldingen gedaan.
-                            </StyledText>
-                        )}
+
                     </View>
                 </View>
             </ScrollView>
@@ -62,7 +27,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#F7F7FC',
         height: '100%',
     },
-    tickets: {
+    ticketDetails: {
         justifyContent: 'center',
         alignItems: 'center',
         flex: 1,
@@ -74,30 +39,6 @@ const styles = StyleSheet.create({
     },
     pageTitle: {
         marginBottom: window.height / 40,
-    },
-
-    addButton: {
-        marginVertical: 15,
-        alignItems: 'center'
-    },
-    addButtonIconWrapper: {
-        backgroundColor: '#A0CAE8',
-        borderRadius: 50,
-        width: window.width / 10 * 1.1,
-        height: window.width / 10 * 1.1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    addButtonText: {
-        marginTop: 4,
-        fontSize: 11,
-        color: 'black'
-    },
-
-    noTickets: {
-        color: 'black',
-        marginTop: '15%',
-        opacity: 0.4
     }
 })
 
