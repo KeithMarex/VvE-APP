eea893d6ac75c67f5e9d3b64ed4b468d57733587
Jan <s1120303@student.hsleiden.nl>
2021-05-22 18:06:25 +0200

    Fixed req.locals

--- API/middleware/IsAuth.ts
+++ API/middleware/IsAuth.ts
@@ -13,13 +13,12 @@ export const isAuth = async (req, res, next) => {
             if (!newTokens.error) {
                 res.cookie('access-token', newTokens.accesstoken, { maxAge: 3 * 60 * 60 * 1000 , httpOnly: true, secure: false });
                 res.cookie('refresh-token', newTokens.refreshToken, { maxAge: 60 * 60 * 24 * 40 * 1000 , httpOnly: true, secure: false });
-                req.locals.user = newTokens.user;
+                req.locals = {"user": newTokens.user};
                 return next();
             } else {
                 return res.status(401).json({ message: newTokens.message });
             }
         } else {
-            console.log("This is the authorizedData: ", authorizedData)
             req.locals.user = authorizedData.user;
             next();
         }
