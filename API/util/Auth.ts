44b0bd6e89b939f35c9de084de48fb20fd1b2975
Jan <s1120303@student.hsleiden.nl>
2021-05-22 17:39:12 +0200

    Fixed getting _id user

--- API/util/Auth.ts
+++ API/util/Auth.ts
@@ -18,7 +18,7 @@ export const refreshTokens = async (refreshToken) => {
         return { error: true, message: err };
     }
 
-    const updatedUser = await User.findById(user.id);
+    const updatedUser = await User.findById(user.user._id);
 
     try {
         jwt.verify(refreshToken, process.env.REFRESH_TOKEN);
