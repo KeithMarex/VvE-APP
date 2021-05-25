27b49c3fcc2afc9e8eeb94b5aaa8ab819e4f5027
Jannyboy313 <janvanoverbeek12@gmail.com>
2021-05-25 14:23:41 +0200

    Added imagesConvert middleware to post

--- API/routes/TicketRouter.ts
+++ API/routes/TicketRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { postTicket, getTickets, getTicket, putTicket } from '../controllers/TicketController';
+import { imagesConvert } from '~/middleware/ImagesConverting';
 
 const router = Router();
 
-router.post('/', postTicket);
+router.post('/', imagesConvert, postTicket);
 router.get('/', getTickets);
 router.get('/:id', getTicket);
 router.put('/:id', putTicket);
