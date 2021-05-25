542067c6fc07a76b7ec1aee14a04d2397d49ab61
Jeroen <jegbol01@gmail.com>
2021-05-25 14:54:54 +0200

    ApiHelper to config

--- MobileApp-New/src/util/ApiHelper.js
+++ MobileApp-New/src/util/ApiHelper.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://vve-api.janvanoverbeek.nl/',
+    baseURL: 'http://192.168.0.105:3001',
     headers: {'Content-Type': 'application/json'}
 })
