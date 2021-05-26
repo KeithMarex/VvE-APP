c7fdf55364dc7da7240791e8452407039e4fd56d
Jannyboy313 <janvanoverbeek12@gmail.com>
2021-05-26 13:26:30 +0200

    Added debug

--- API/middleware/ImagesConverting.ts
+++ API/middleware/ImagesConverting.ts
@@ -2,7 +2,7 @@ import { NextFunction, Response } from 'express';
 import Image from '~/models/Image';
 import logger from '~/util/Logger';
 import path from 'path'
-
+import fs from 'fs';
 // export const imagesConvert = async(req, res, next) => {
 //     if (!req.body.images || !req.body.images.length ) {
 //         return next();
@@ -26,21 +26,31 @@ import path from 'path'
 //     return next()
 // }
 
-const createImages = (files) => {
-    let imageModels: Array<any> = [];
+// function getBase64(file) {
+//     file.readFileSync(encodeBase64);
+//  }
+
+const createImages = async(files) => {
+    let imageIds: Array<any> = [];
     try {
         for(let key in files) {
             if (files.hasOwnProperty(key)) {
                 let file = files[key]
-                // imageModels.push(new Image(files[key][i]));
+                // let base64 = await imageToBase64(file.path);
+                let base64 = fs.readFileSync(file.path, {encoding: 'base64'});
+                // Kijken of base 64 opgeslagen kan worden en of ik deze terug naar image kan brengen.
+                console.log("This is the base64: ",base64);
+                let image = new Image({ "name": file.name, "image": base64 })
+                // let imageModel = await image.save();
+                // imageIds.push(imageModel._id);
             }
         }
     } catch (err) {
         logger.error(err);
-        return err;
+        return {err: true, message: err};
     }
 
-    return imageModels;
+    return {err: false, images: imageIds};
 }
 
 const isImage = (files: any) => {
@@ -57,17 +67,21 @@ const isImage = (files: any) => {
     return true;
 }
 
-export const imagesConvert = (req: any, res: Response, next: NextFunction) => {
+export const imagesConvert = async(req: any, res: Response, next: NextFunction) => {
 
     if (JSON.stringify(req.files)==JSON.stringify({})) {
         return next();
     }
 
     if(isImage(req.files)) {
-        // Upload image
-        next();
+        const imageIds = await createImages(req.files);
+        if (imageIds.err === true) {
+            return res.status(500).json({message: imageIds.message})
+        }
+        return res.status(200).json({message: "Vgm werkt het"})
+        // return next();
     }else {
-        res.status(400).json({message: "Only images are allowed"});
+        return res.status(400).json({message: "Only images are allowed"});
     }
     res.end();
 
