7e076874560c50646ef99c34b4a737865710670b
Jannyboy313 <janvanoverbeek12@gmail.com>
2021-05-26 14:53:40 +0200

    Replaced respond with next

--- API/middleware/ImagesConverting.ts
+++ API/middleware/ImagesConverting.ts
@@ -68,8 +68,7 @@ export const imagesConvert = async(req: any, res: Response, next: NextFunction) 
         if (imageIds.err === true) {
             return res.status(500).json({message: imageIds.message})
         }
-        return res.status(200).json({message: "Vgm werkt het"})
-        // return next();
+        return next();
     }else {
         return res.status(400).json({message: "Only images are allowed"});
     }
