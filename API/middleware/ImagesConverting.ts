6247c0636f27bf9b8b65a6303a90fe41fa752b1d
Jannyboy313 <janvanoverbeek12@gmail.com>
2021-05-26 15:39:36 +0200

    Added image ids to res.locals

--- API/middleware/ImagesConverting.ts
+++ API/middleware/ImagesConverting.ts
@@ -68,6 +68,7 @@ export const imagesConvert = async(req: any, res: Response, next: NextFunction) 
         if (imageIds.err === true) {
             return res.status(500).json({message: imageIds.message})
         }
+        res.locals.images = imageIds;
         return next();
     }else {
         return res.status(400).json({message: "Only images are allowed"});
