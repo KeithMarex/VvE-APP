cf080d72aed22f73ebbebf6d3c6b0eddb55bbb95
Jannyboy313 <janvanoverbeek12@gmail.com>
2021-05-26 16:29:53 +0200

    Added slash to uri

--- API/routes/UserRouter.ts
+++ API/routes/UserRouter.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.post('/login', login);
 router.post('/register', register);
 router.get('/:id', isAuth, getUser);
-router.get('/organization:id', isAuth, getUsersOrganization)
+router.get('/organization/:id', isAuth, getUsersOrganization)
 // router.put('/:id', userController.putUser);
 // router.delete('/:id', userController.deleteUser);
 
