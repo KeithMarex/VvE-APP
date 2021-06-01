d918531e092f6e6aca11ed97c9b8eb79f6a5a9c0
SS <xcyberxx@outlook.com>
2021-06-01 13:35:59 +0200

    Model, post en halve get.

---
+++ API/models/Agenda_item.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+// Aanmaken datum validator en importen -> use
+
+const Schema = mongoose.Schema;
+
+const AgendaSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'Title for Agenda_item is required'],
+        trim: true
+    },
+    description: {
+        type: String,
+        required: [true, 'A description is required for Agenda_item'],
+        trim: true
+    },
+    date: {
+        type: Date,
+        required: [true, 'A date/time is required for Agenda_item'],
+        // validate: { // Wellicht later toevoegen
+        //     validator: validateDate,
+        //     message: 'Please give a valid date/time'
+        // }
+    }
+}, {timestamps: true});
+
+
+export default mongoose.model('Agenda_items', AgendaSchema);
