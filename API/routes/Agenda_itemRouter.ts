fb84a300a37138e7174e98f4d5d391625331f5de
SScheenstra <xcyberxx@outlook.com>
2021-06-01 17:46:24 +0200

    isAdmin Check

--- API/routes/Agenda_itemRouter.ts
+++ API/routes/Agenda_itemRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getAgenda, postAgenda } from '~/controllers/AgendaController';
+import { isAdmin } from '~/middleware/IsAdmin';
 
 const router = Router();
 
-router.post('/', postAgenda);
+router.post('/', isAdmin, postAgenda);
 router.get('/:from/:to', getAgenda);
 // router.get('/:id', getTicket);
 // router.put('/:id', putTicket);
