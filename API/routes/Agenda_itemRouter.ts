25e069cc6212624a825103a3a46dbd55bb562189
SScheenstra <xcyberxx@outlook.com>
2021-06-02 17:48:05 +0200

    Details endpoint toegevoegd

--- API/routes/Agenda_itemRouter.ts
+++ API/routes/Agenda_itemRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { deleteAgenda, getAgenda, getAgendaNext, postAgenda, putAgenda } from '~/controllers/AgendaController';
+import { deleteAgenda, getAgenda, getAgendaDetails, getAgendaNext, postAgenda, putAgenda } from '~/controllers/AgendaController';
 import { isAdmin } from '~/middleware/IsAdmin';
 
 const router = Router();
 
 router.get('/next', getAgendaNext);
 router.get('/:month', getAgenda);
+router.get('/details/:id', getAgendaDetails);
 router.post('/', isAdmin, postAgenda);
 router.put('/:id', isAdmin, putAgenda);
 router.delete('/:id', isAdmin, deleteAgenda);
