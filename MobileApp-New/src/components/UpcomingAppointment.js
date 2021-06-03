8434f23ccbb576fadba549a5ea0af36c54189a99
KeithMarex <koenvandermarel@gmail.com>
2021-06-03 12:26:34 +0200

    [Added] Getting upcoming appointment

--- MobileApp-New/src/components/UpcomingAppointment.js
+++ MobileApp-New/src/components/UpcomingAppointment.js
@@ -1,16 +1,26 @@
 import {Dimensions, StyleSheet, View} from "react-native";
 import StyledText from "./StyledText";
 import {CalendarIcon} from "../resources";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import ApiHelper from "../util/ApiHelper";
+import moment from "moment";
 
 const UpcomingAppointment = () => {
+    const [upcomingApp, setUpcomingApp] = useState({});
+    useEffect(() => {
+        // Function to get API Data
+        ApiHelper.get('/agenda/next').then(val => {
+            setUpcomingApp(val);
+        })
+    });
+
     return (
         <View style={styles.upcomingAppointment}>
             <View style={{width: Dimensions.get('window').width * .7,}}>
             <StyledText>Eerst volgende nieuwe afspraak</StyledText>
             <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', marginTop: 10}}>
                 <CalendarIcon stroke={'#451864'}/>
-                <StyledText inputStyle={styles.informatie}>Zaterdag 19 Juni 16:00 - 17:30</StyledText>
+                <StyledText inputStyle={styles.informatie}>{moment(upcomingApp['date'].split('T', 1), "YYYY-MM-DD").format("DD-MMMM-YYYY")}</StyledText>
             </View>
         </View>
     </View>
