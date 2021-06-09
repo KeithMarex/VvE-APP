58fe277ed354ce47ecff6f740d09f9ee1afd49e1
Jan <s1120303@student.hsleiden.nl>
2021-06-09 10:58:26 +0200

    Removed isAuth

--- API/routes/OrganizationRouter.ts
+++ API/routes/OrganizationRouter.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import { getUsersOrganization } from '../controllers/OrganizationController';
-import { isAuth } from '../middleware/IsAuth';
 import { isAdmin } from '../middleware/IsAdmin';
 
 const router = Router();
 
-router.get('/users', isAuth, isAdmin, getUsersOrganization);
+router.get('/users', isAdmin, getUsersOrganization);
 
 export default router;
