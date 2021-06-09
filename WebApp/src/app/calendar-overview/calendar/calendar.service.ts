98f31aa792edcab952224318a8b2a4c41e850de0
Jeroen <jegbol01@gmail.com>
2021-06-09 10:13:19 +0200

    Created a CalendarItem <> CustomEvent parser in the calendar service

--- WebApp/src/app/calendar-overview/calendar/calendar.service.ts
+++ WebApp/src/app/calendar-overview/calendar/calendar.service.ts
@@ -1,12 +1,60 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { AgendaItem } from '../../../shared/models/agenda-item';
+import {CustomEvent} from './custom-event';
 
 @Injectable()
 export class CalendarService {
   calendarItems = new BehaviorSubject<AgendaItem[]>([]);
+  colors: any = {
+    red: {
+      primary: '#ad2121',
+      secondary: '#FAE3E3',
+    },
+    blue: {
+      primary: '#1e90ff',
+      secondary: '#D1E8FF',
+    },
+    yellow: {
+      primary: '#e3bc08',
+      secondary: '#FDF1BA',
+    },
+  };
 
   constructor() { }
+
+  customEventToCalendarItem(event: CustomEvent): AgendaItem {
+    return new AgendaItem(
+      event.id,
+      event.title,
+      event.description,
+      event.start,
+      event.end
+    );
+  }
+
+  calendarItemToCustomEvent(calItem: AgendaItem, actions): CustomEvent {
+    const startDate = new Date(calItem.date);
+    const endDate = calItem.endDate ? new Date(calItem.endDate) : undefined;
+
+    const customEvent: CustomEvent[] = [];
+    customEvent.push({
+      start: startDate,
+      end: endDate,
+      title: calItem.title,
+      description: calItem.description,
+      id: calItem._id,
+      color: this.colors.blue,
+      allDay: !!calItem.endDate,
+      actions,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true,
+      },
+      draggable: true,
+    });
+    return customEvent[0];
+  }
 
   setCalendarItems(calendarItems: AgendaItem[]): void {
     this.calendarItems.next(calendarItems);
