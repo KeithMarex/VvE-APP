0ff1bb841e14d6ed4976bc5b7e6a521828128f73
Jeroen <jegbol01@gmail.com>
2021-06-09 12:38:00 +0200

    Implemented simple date parser

--- WebApp/src/app/calendar-overview/calendar/calendar-item-details/calendar-item-details.component.ts
+++ WebApp/src/app/calendar-overview/calendar/calendar-item-details/calendar-item-details.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import moment from 'moment';
+import { isSameDay } from 'date-fns';
 import { CalendarItem } from '../../../../shared/models/calendar-item';
 
 @Component({
@@ -8,10 +10,36 @@ import { CalendarItem } from '../../../../shared/models/calendar-item';
 })
 export class CalendarItemDetailsComponent implements OnInit {
   @Input() calendarItem: CalendarItem;
+  dateString: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.createDateString();
   }
 
+  createDateString(): void {
+    let parsedDate = '';
+    const startDate = this.calendarItem.date;
+    const endDate = this.calendarItem.endDate;
+
+    const parsedStartDate = this.parseDate(startDate);
+    const parsedEndDate = this.parseDate(endDate);
+
+    console.log(startDate);
+    console.log(endDate);
+    if (isSameDay(startDate, endDate)) {
+      parsedDate = `${parsedStartDate.weekDay} ${parsedStartDate.monthDate} - ${parsedStartDate.time} / ${ parsedEndDate.time}`;
+    }
+    // parsedDate = startWeekDay + ' ' + startMonthDay + ' - ' + startTime;
+    this.dateString = parsedDate;
+  }
+
+  parseDate(date: Date): { weekDay, monthDate, time } {
+    return {
+      weekDay: moment(date).format('dddd'),
+      monthDate: moment(date).format('D MMMM'),
+      time: moment(date).format('LT')
+    };
+  }
 }
