61f776ee269764569fb036f21554da71afa81b27
Jan <s1120303@student.hsleiden.nl>
2021-06-09 10:04:43 +0200

    Added middleware for status check

---
+++ API/middleware/StatusConverter.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Response } from "express";
+
+export const statusConverter = (req, res: Response, next: NextFunction) => {
+    if (!req.body.status) {
+        return next();
+    }
+
+    req.body.status.toUpperCase();
+    const status = req.body.status;
+
+    if (status === "PENDING" || status === "HANDLING" || status === "HANDLED") {
+        return next();
+    }
+
+    return res.status(400).json({ message: 'Status that is incorrect must be: "PENDING", "HANDLING" or "HANDLED otherwise not accepted' });
+}
