be9342e91c8e668cedb28cad0f14bbf2e69c81aa
Jan <s1120303@student.hsleiden.nl>
2021-06-09 09:53:48 +0200

    Added assignee check

--- API/validators/TicketPutValidator.ts
+++ API/validators/TicketPutValidator.ts
@@ -3,7 +3,7 @@ export function validateBodyPutTicket(body: any) {
     if(body.status)
         newBody.status = body.status;
     if(body.assignee)
-        newBody.status = body.status;
+        newBody.assignee = body.assignee;
     if(body.tag)
         newBody.tag = body.tag;
     return newBody;
