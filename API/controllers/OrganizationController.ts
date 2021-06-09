1686546e8c345336fb675d3e19d85b44124087e1
Jan <s1120303@student.hsleiden.nl>
2021-06-09 12:05:48 +0200

    Added put function for theme

--- API/controllers/OrganizationController.ts
+++ API/controllers/OrganizationController.ts
@@ -3,6 +3,7 @@ import Organization from '../models/Organization';
 import logger from '~/util/Logger';
 import { Types } from 'mongoose';
 import { Response } from 'express';
+import { validateTheme } from '../validators/ThemeValidator';
 
 export const getUsersOrganization = (req, res: Response) => {
     User.find({
@@ -26,5 +27,17 @@ export const getOrganizationTheme = (req, res: Response ) => {
     .catch( err => {
         logger.error(err);
         res.status(500).json({ message: err })
+    })
+}
+
+export const putOrganizationTheme = (req, res: Response ) => {
+    const body = validateTheme(req.body);
+    Organization.updateOne({ _id: res.locals.user.organizations[0] }, body)
+    .then ( () => {
+        res.status(200);
+    })
+    .catch( err => {
+        logger.error(err);
+        res.status(400).json({ message: err })
     })
 }
