9c1c400e865bfccec16f43cb80fb14d33fcc3dd4
Jan <s1120303@student.hsleiden.nl>
2021-06-09 10:04:27 +0200

    Removed toUpperCase

--- API/validators/TicketPutValidator.ts
+++ API/validators/TicketPutValidator.ts
@@ -1,7 +1,7 @@
 export function validateBodyPutTicket(body: any) {
     let newBody: any = {};
     if(body.status)
-        newBody.status = body.status;
+        newBody.status = body.status
     if(body.assignee)
         newBody.assignee = body.assignee;
     if(body.tag)
