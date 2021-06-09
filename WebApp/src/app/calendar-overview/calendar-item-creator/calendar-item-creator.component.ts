4ac57a52230c577575f84dd4352d9305d7ea1c7f
Jeroen <jegbol01@gmail.com>
2021-06-09 10:13:53 +0200

    Load data in editor

--- WebApp/src/app/calendar-overview/calendar-item-creator/calendar-item-creator.component.ts
+++ WebApp/src/app/calendar-overview/calendar-item-creator/calendar-item-creator.component.ts
@@ -1,9 +1,10 @@
-import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { DaterangepickerComponent, DaterangepickerDirective } from 'ngx-daterangepicker-material';
+import { DaterangepickerComponent } from 'ngx-daterangepicker-material';
 import { CalendarDao } from 'src/shared/services/calendar-dao.service';
 import { CalendarService } from '../calendar/calendar.service';
 import moment from 'moment';
+import { CustomEvent } from '../calendar/custom-event';
 moment.locale('nl');
 
 @Component({
@@ -12,32 +13,48 @@ moment.locale('nl');
   styleUrls: ['./calendar-item-creator.component.scss']
 })
 export class CalendarItemCreatorComponent implements OnInit {
-  @Output() calendarItemCreated = new EventEmitter();
+  @Output() calendarItemCreated: EventEmitter<void> = new EventEmitter();
+  @Input() isEditing: boolean;
+  @Input() calendarItemToEdit: CustomEvent;
   selectedDateTime: {startDate: moment.Moment, endDate: moment.Moment};
   inlineDate: any;
   picker: DaterangepickerComponent;
   locale: {};
+  title: string;
+  description: string;
 
   constructor(private calendarDao: CalendarDao, private calendarService: CalendarService) { }
 
   ngOnInit(): void {
-    this.selectedDateTime = {
-      startDate: moment().startOf('day'),
-      endDate: moment().startOf('day')
-    };
     this.locale = {
       daysOfWeek: moment.weekdaysMin(),
       monthNames: moment.months(),
       firstDay: moment.localeData().firstDayOfWeek()
     };
+
+    this.calendarItemToEdit ? this.initEditItem() : this.initCreateItem();
   }
 
-  onCreateItem(form: NgForm): void {
-    const { title, description } = form.value;
+  initCreateItem(): void {
+    this.selectedDateTime = {
+      startDate: moment().startOf('day'),
+      endDate: moment().startOf('day')
+    };
+  }
 
+  initEditItem(): void {
+    this.title = this.calendarItemToEdit.title;
+    this.description = this.calendarItemToEdit.description;
+    this.selectedDateTime = {
+      startDate: moment(this.calendarItemToEdit.start),
+      endDate: moment(this.calendarItemToEdit.end)
+    };
+  }
+
+  onSave(form: NgForm): void {
+    const { title, description } = form.value;
     const startDate = this.selectedDateTime.startDate.toDate();
     const endDate = this.selectedDateTime.endDate.toDate();
-
     const payload = {
       title,
       description,
@@ -45,6 +62,10 @@ export class CalendarItemCreatorComponent implements OnInit {
       enddate: endDate
     };
 
+    if (this.isEditing) {
+      // this.calendarDao.updateCalendarItem(this.calendarItemToEdit);
+      return;
+    }
     this.calendarDao.createCalendarItem(payload)
       .subscribe((newCalendarItem) => {
         this.calendarService.addCalendarItem(newCalendarItem);
