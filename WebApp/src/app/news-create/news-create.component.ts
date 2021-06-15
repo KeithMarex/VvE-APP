a202a407f39ff4a4e0a41f1ccea1295fd307667a
Jan <s1120303@student.hsleiden.nl>
2021-06-15 11:03:08 +0200

    Generated news-create component

---
+++ WebApp/src/app/news-create/news-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-create',
+  templateUrl: './news-create.component.html',
+  styleUrls: ['./news-create.component.scss']
+})
+export class NewsCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
