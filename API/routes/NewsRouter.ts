d331203b860fc573798d03a64531ff4240d63fca
SS <xcyberxx@outlook.com>
2021-06-17 00:07:22 +0200

    Removed comments

--- API/routes/NewsRouter.ts
+++ API/routes/NewsRouter.ts
@@ -9,10 +9,5 @@ router.post('/', isAdmin, postNews);
 router.get('/', getAllNews);
 router.get('/:id', getNews);
 router.delete('/:id', isAdmin, deleteNews);
-// router.post('/', postTag);
-// router.get('/', getTags);
-// router.get('/:id', getTag);
-// router.put('/:id', putTag);
-// router.delete('/:id', deleteTag);
 
 export default router;
