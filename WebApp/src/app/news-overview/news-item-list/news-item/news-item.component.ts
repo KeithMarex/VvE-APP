832f7538be7fe67273e82f7661fb5fd5efc553fe
Jeroen <jegbol01@gmail.com>
2021-06-21 15:00:19 +0200

    Fetched news items are properly showed

--- WebApp/src/app/news-overview/news-item-list/news-item/news-item.component.ts
+++ WebApp/src/app/news-overview/news-item-list/news-item/news-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import moment from 'moment';
+import { NewsItem } from '../../../../shared/models/news-item';
 
 @Component({
   selector: 'app-news-item',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-item.component.scss']
 })
 export class NewsItemComponent implements OnInit {
+  @Input() newsItem: NewsItem;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.newsItem);
   }
 
+  parseDate(date: Date): string {
+    return moment(date).format('llll');
+  }
 }
--- WebApp/src/shared/models/news-item.ts
+++ WebApp/src/shared/models/news-item.ts
@@ -4,12 +4,14 @@ export class NewsItem {
     author: string;
     content: string;
     organization: object;
+    updatedAt: Date;
 
-    constructor(_id: string, title: string, content: string, author: string, organization: object) {
+  constructor(_id: string, title: string, content: string, author: string, organization: object, updatedAt: Date) {
         this._id = _id;
         this.title = title;
         this.content = content;
         this.author = author;
         this.organization = organization;
+        this.updatedAt = updatedAt;
     }
 }
