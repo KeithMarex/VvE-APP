c044c556b01842708d90871ffa4dc6bca4d2b5be
Jeroen <jegbol01@gmail.com>
2021-06-21 15:52:34 +0200

    Can delete news

--- WebApp/src/app/news-overview/news-item-list/news-item-list.component.ts
+++ WebApp/src/app/news-overview/news-item-list/news-item-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsDao } from '../../../shared/services/news-dao.service';
 import { NewsItem } from '../../../shared/models/news-item';
+import News from "../../../../../API/models/News";
 
 @Component({
   selector: 'app-news-item-list',
@@ -22,6 +23,14 @@ export class NewsItemListComponent implements OnInit {
     this.newsDao.getNewsItems()
       .subscribe((newsItems) => {
         this.newsItems = newsItems;
+      });
+  }
+
+  deleteNewsItem(newsItem: NewsItem): void {
+    this.newsDao.deleteCalendarItem(newsItem._id)
+      .subscribe(() => {
+        const removeIndex = this.newsItems.map(item => item._id).indexOf(newsItem._id);
+        this.newsItems.splice(removeIndex, 1);
       });
   }
 
--- WebApp/src/app/news-overview/news-item-list/news-item/news-item.component.ts
+++ WebApp/src/app/news-overview/news-item-list/news-item/news-item.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import moment from 'moment';
 import { NewsItem } from '../../../../shared/models/news-item';
+import { NewsDao } from '../../../../shared/services/news-dao.service';
 
 @Component({
   selector: 'app-news-item',
@@ -9,11 +10,16 @@ import { NewsItem } from '../../../../shared/models/news-item';
 })
 export class NewsItemComponent implements OnInit {
   @Input() newsItem: NewsItem;
+  @Output() deleteNewsItem = new EventEmitter<NewsItem>();
 
   constructor() { }
 
   ngOnInit(): void {
     console.log(this.newsItem);
+  }
+
+  onDeleteNewsItemClicked(): void {
+    this.deleteNewsItem.emit(this.newsItem);
   }
 
   parseDate(date: Date): string {
