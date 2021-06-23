e6d35dce72695e15594c150f5f18c714d0d4bd89
Jeroen <jegbol01@gmail.com>
2021-06-23 21:01:06 +0200

    Translations are instant and refactored code

--- MobileApp-New/src/config/languages/translate.js
+++ MobileApp-New/src/config/languages/translate.js
@@ -1,23 +1,21 @@
-import translations from '../languages/translations.json';
-import { NativeModules, Platform, AsyncStorage } from "react-native";
+import translations from '../languages/translations.json'
+import { NativeModules, Platform, AsyncStorage } from 'react-native'
+
+let language
 
 const translate = async () => {
-    let locale = ""
-    const hasLang = await AsyncStorage.getItem('lang');
-
-    if (hasLang != null){
-        locale = hasLang;
-    } else {
-        if (Platform.OS === 'ios'){
-            locale = NativeModules.SettingsManager.settings.AppleLocale.split('_', 1) || NativeModules.SettingsManager.settings.AppleLanguages[0].split('_', 1);
-        } else {
-            locale = NativeModules.I18nManager.localeIdentifier.split('_', 1);
-        }
-        locale = locale[0];
+    if (!language) {
+        language = await AsyncStorage.getItem('lang')
     }
 
-    return translations[locale];
+    return translations[language ? language : fetchSystemLocale]
 }
 
-export default translate;
+const fetchSystemLocale = () => {
+    return Platform.OS === 'ios'
+        ? (NativeModules.SettingsManager.settings.AppleLocale.split('_', 1) || NativeModules.SettingsManager.settings.AppleLanguages[0].split('_', 1))[0]
+        : (NativeModules.I18nManager.localeIdentifier.split('_', 1))[0]
+}
+
+export default translate
 
