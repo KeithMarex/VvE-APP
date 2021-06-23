f9a7e48a3f5a282909887da3f6e3301d82897633
Yaps551 <65169192+Yaps551@users.noreply.github.com>
2021-06-23 14:37:23 +0200

    Added filter functionality. Filter button is now filter/sort button

--- WebApp/src/app/ticket-overview/ticket-list/ticket-list.component.ts
+++ WebApp/src/app/ticket-overview/ticket-list/ticket-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ticket } from 'src/shared/models/ticket.model';
 import { TicketDao } from 'src/shared/services/ticket-dao.service';
 import { sortBy } from 'sort-by-typescript';
@@ -10,7 +10,8 @@ import { sortBy } from 'sort-by-typescript';
 })
 export class TicketListComponent implements OnInit {
   tickets: Ticket[] = [];
-  filteredTickets: Ticket[];
+  filteredTickets: Ticket[] = this.tickets;
+  noFilteredTickets: boolean = false;
 
   constructor(
     private ticketDao: TicketDao,
@@ -30,5 +31,30 @@ export class TicketListComponent implements OnInit {
 
   onChangeSort(sortProperty: string): void {
     this.tickets = this.tickets.sort(sortBy('-' + sortProperty));
+  }
+
+  onChangeFilter(filterTerm: string): void {
+    this.noFilteredTickets = false;
+    var ticketsWithFilterTerm: Ticket[] = [];
+
+    var tickets = this.tickets;
+
+    for (var i = 0; i < tickets.length; i++) {
+      const ticketAsString = JSON.stringify(tickets[i]).toLowerCase(); // Fully stringify ticket object
+
+      if (ticketAsString.includes(filterTerm.toLocaleLowerCase())) {
+        ticketsWithFilterTerm.push(tickets[i]);
+      }
+    }
+
+    if (ticketsWithFilterTerm.length > 0) {
+      this.filteredTickets = ticketsWithFilterTerm;
+    }
+    else if (filterTerm.length == 0) {
+      this.filteredTickets = this.tickets;
+    }
+    else {
+      this.noFilteredTickets = true;
+    }
   }
 }
